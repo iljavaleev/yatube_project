c51a9b8ab555374a61dd4fd6818f803a4a34ca20
Ilya Valeev <iljavaleev@gmail.com>
2022-03-01 15:13:38 +0300

    add some views and urls

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Главная страница
+def index(request):    
+    return HttpResponse('Main page')
+
+
+    # Страница со списком мороженого
+def group_posts(request, slug):
+    return HttpResponse('Group posts with slug {}'.format(slug))
+
+
 
 # Create your views here.
